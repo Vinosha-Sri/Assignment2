c25b7ac1c57a755c435b74912f72795d183d4b05
Vinosha Srikugananthan <vinosha.srikugananthan@ontariotechu.net>
2024-11-05 09:59:48 -0500

    Commit 2.12

--- myportfolio/routes/index.js
+++ myportfolio/routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
-
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {

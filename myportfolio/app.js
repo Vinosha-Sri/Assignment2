1f098afdd3357a49433a0a91c451c5befbb10777
Vinosha Srikugananthan <vinosha.srikugananthan@ontariotechu.net>
2024-11-05 20:36:19 -0500

    Commit 2.20

--- myportfolio/app.js
+++ myportfolio/app.js
@@ -1,50 +1,47 @@
+// Installed 3rd part packaages
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
+
 let app = express();
 
-// Set up view engine
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
-// Routes
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
-});
+app.use('/', indexRouter); //localhost:3000
+app.use('/users', usersRouter); //localhost:3000/users
 
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
-});
-
-app.get('/projects', (req, res) => {
-  res.render('projects', { title: 'Projects' });
-});
-
-app.get('/contact', (req, res) => {
-  res.render('contact', { title: 'Contact' });
-});
-
-// Catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Error handler
+// error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
-  res.render('error', { title: 'Error' });
+  res.render('error',
+  {
+    title:"Error"
+  }
+  );
 });
 
 module.exports = app;
